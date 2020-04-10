5af58571cdf4c4bcc936e2031f15854c2aa0df50
LSW <lycox1@gmail.com>
2020-04-11 00:01:06 +0900

    2020-04-11 Sleep Alarm Setting menu under writing

--- app/src/main/java/com/example/sweetsnore/ui/SleepAlarmSettingActivity.java
+++ app/src/main/java/com/example/sweetsnore/ui/SleepAlarmSettingActivity.java
@@ -1,13 +1,16 @@
-package com.example.sweetsnore;
+package com.example.sweetsnore.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.sweetsnore.R;
+
+public class SleepAlarmSettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sleep_alarm_setting);
     }
 }
