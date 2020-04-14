b96e5e89dc32e9ba9e31b122c5348d30d334c62a
LSW <lycox1@gmail.com>
2020-04-14 13:48:27 +0900

    2020-04-14 Matching display size

--- app/src/main/java/com/example/sweetsnore/MainActivity.java
+++ app/src/main/java/com/example/sweetsnore/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.sweetsnore;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    static String TAG = "SweetSnore_MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "display density " + getResources().getDisplayMetrics().density);
+        Log.d(TAG, "display dpi " + getResources().getDisplayMetrics().densityDpi);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        // 해상도 구하는 방법
+        float widthDp = dm.widthPixels / dm.density;
+
+        String str_ScreenSize = "The Android Screen is: "
+                + dm.widthPixels
+                + " x "
+                + dm.heightPixels + "\n"
+                + "Width DP : " + widthDp;
+        Log.d(TAG, str_ScreenSize);
     }
 }
